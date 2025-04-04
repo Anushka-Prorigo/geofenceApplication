8b126e06581f23221a7bb5512ec979fa8dac74be
Anushka Patil <anushka.patil@prorigosoftware.com>
2025-04-04 18:26:43 +0530

    changes made in react native

--- android/app/src/main/java/com/geofenceapp/MainApplication.kt
+++ android/app/src/main/java/com/geofenceapp/MainApplication.kt
@@ -1,6 +1,6 @@
 package com.geofenceapp
-
 import android.app.Application
+import com.geofenceapplication.GeofencePackage
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -20,6 +20,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+               add(GeofencePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
